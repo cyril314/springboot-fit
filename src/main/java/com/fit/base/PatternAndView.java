1b49f4c7ea760613e0ef006200d93791343a8da7
cyril314 <svip.cyril@gmail.com>
2025-01-13 16:37:28 +0800

    Initial commit

--- src/main/java/com/fit/base/PatternAndView.java
+++ src/main/java/com/fit/base/PatternAndView.java
@@ -1,12 +1,11 @@
 package com.fit.base;
 
-import com.fit.util.ConverterUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.HashMap;
@@ -18,23 +17,11 @@ import java.util.Map;
  * @DATE 2017-9-14 下午10:05:40
  * @Author AIM
  */
+@Slf4j
 public class PatternAndView extends ModelAndView {
 
-    private Logger logger = LoggerFactory.getLogger(PatternAndView.class);
-
     public PatternAndView(String viewName) {
-        super.setViewName(viewName);
-    }
-
-    public PatternAndView(String viewName, HttpServletRequest request, HttpServletResponse response) {
-        String contextPath = request.getContextPath().equals("/") ? "" : request.getContextPath();
-        String webPath = getURL(request);
-        String port = ":" + ConverterUtils.toInt(Integer.valueOf(request.getServerPort()));
-        if (generic_domain(request).equals("127.0.0.1")) {
-        } else if (!generic_domain(request).equals("localhost")) {
-            //webPath = "http://www." + generic_domain(request) + port + contextPath;
-            webPath = webPath + port + contextPath;
-        }
+        String webPath = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
         super.setViewName(viewName);
         super.addObject("webpath", webPath);
     }
@@ -60,41 +47,10 @@ public class PatternAndView extends ModelAndView {
         IP = trimSpaces(IP);
         if (IP.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")) {
             String[] s = IP.split("\\.");
-            if ((Integer.parseInt(s[0]) < 255) && (Integer.parseInt(s[1]) < 255) && (Integer.parseInt(s[2]) < 255)
-                    && (Integer.parseInt(s[3]) < 255))
+            if ((Integer.parseInt(s[0]) < 255) && (Integer.parseInt(s[1]) < 255) && (Integer.parseInt(s[2]) < 255) && (Integer.parseInt(s[3]) < 255))
                 b = true;
         }
         return b;
-    }
-
-    /**
-     * 获取请求的域名或IP
-     */
-    protected String generic_domain(HttpServletRequest request) {
-        String system_domain = "localhost";
-        String serverName = request.getServerName();
-        if (isIp(serverName))
-            system_domain = serverName;
-        else {
-            system_domain = serverName.substring(serverName.indexOf(".") + 1);
-        }
-
-        return system_domain;
-    }
-
-    /**
-     * 请求地址
-     */
-    protected String getURL(HttpServletRequest request) {
-        String contextPath = request.getContextPath().equals("/") ? "" : request.getContextPath();
-        String url = "http://" + request.getServerName();
-        if (ConverterUtils.toInt(Integer.valueOf(request.getServerPort())) != 80)
-            url = url + ":" + ConverterUtils.toInt(Integer.valueOf(request.getServerPort())) + contextPath;
-        else {
-            url = url + contextPath;
-        }
-        // logger.info("请求地址为："+url);
-        return url;
     }
 
     /**
@@ -109,7 +65,7 @@ public class PatternAndView extends ModelAndView {
         try {
             ret = URLDecoder.decode(s.trim(), coding);
         } catch (Exception localException) {
-            logger.info("URL解码异常");
+            log.info("URL解码异常");
         }
         return ret;
     }
@@ -126,7 +82,7 @@ public class PatternAndView extends ModelAndView {
         try {
             ret = URLEncoder.encode(s.trim(), coding);
         } catch (Exception localException) {
-            logger.info("URL编码异常");
+            log.info("URL编码异常");
         }
         return ret;
     }
